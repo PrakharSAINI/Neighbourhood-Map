8125776fcd86a2d49bddfac03531c040b96ae184
PRAKHAR K SAINI <prakhar.16m@gmail.com>
2017-06-22 11:20:17 +0530

    Separate data into JSON and zomato API

---
+++ js/zomato.js
@@ -0,0 +1,34 @@
+var zheader,version,url;
+var u = "https://developers.zomato.com/api/"
+var Zomato = {
+  init:function (opts) {
+    if (opts.key!=null) {
+      zheader = {
+        Accept : "text/plain; charset=utf-8",
+        "Content-Type": "text/plain; charset=utf-8",
+        "X-Zomato-API-Key":opts.key
+      }
+    } else {
+      console.error("Enter the key");
+    };
+    version = opts.version||"v2.1";
+    url = u + version
+  },
+
+  restaurant:function (resid,scb,ecb) {
+    if (resid==null) {
+      console.error("Enter the restaurant id correctly");
+    } else {
+      $.ajax({
+        url:url+"/restaurant?res_id=" + resid,
+        headers:zheader,
+        success:function (response) {
+          scb(response);
+        },
+        error:function (res) {
+          ecb(res)
+        }
+      });
+    }
+  }
+}
